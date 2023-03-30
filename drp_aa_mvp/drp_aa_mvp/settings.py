4bbdb35f60891e29ec8179d478c43aeb7b0ba939
Ryan Rix <ryan@whatthefuck.computer>
2023-03-30 11:16:23 -0700

    allow specifying port in postgres env

--- drp_aa_mvp/drp_aa_mvp/settings.py
+++ drp_aa_mvp/drp_aa_mvp/settings.py
@@ -102,7 +102,8 @@ else:
             'NAME': os.environ.get('POSTGRES_NAME') or 'authorizedagent',
             'USER': os.environ.get('POSTGRES_USER') or 'postgres',
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD') or 'rootz',
-            'HOST': os.environ.get('POSTGRES_HOST') or 'localhost'
+            'HOST': os.environ.get('POSTGRES_HOST') or 'localhost',
+            'PORT': os.environ.get('POSTGRES_PORT') or '5432'
         },
     }
 
