dd518b1005cdabf0ea9d988f675c6236468f4484
Ryan Rix <ryan@whatthefuck.computer>
2023-04-03 21:14:25 -0700

    add new models to admin panel

--- drp_aa_mvp/drp_pip/admin.py
+++ drp_aa_mvp/drp_pip/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import AuthorizedAgent
+from .models import AuthorizedAgent, DataRightsRequest, DataRightsStatus
 
 # Register your models here.
 admin.site.register(AuthorizedAgent)
+admin.site.register(DataRightsRequest)
+admin.site.register(DataRightsStatus)
