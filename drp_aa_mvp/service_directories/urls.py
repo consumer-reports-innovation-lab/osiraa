818a89dc5fdf095a44185e0f0d10f7c167251897
Ryan Rix <ryan@whatthefuck.computer>
2023-04-12 12:14:17 -0700

    *splat*

---
+++ drp_aa_mvp/service_directories/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    # path('businesses', views.business_directory, name='businesses'),
+    # path('agents', views.agent_directory, name='agents'),
+]
